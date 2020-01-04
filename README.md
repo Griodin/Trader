5419db4892c903f4c9d032088be966a7958ce700
Matthew C. Stone <seematt@comcast.net>
2020-01-04 16:34:38 -0500

    updated

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ﻿# Trader
 
 
+This doesn't work but I am trying to get Nodejs, requirejs, d3, d3fc to all work together.
+Open to suggestions, I am looking for simple.
+
+for this first part, I am trying to build decent charts to read from a exchange api JSON or possibly back data from a database.
+Inorder to get to the next step I need to figure out how to get the basic chart loaded.
--- server.js
+++ server.js
@@ -1,8 +1,75 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+var http = require('http');
+var fs = require('fs');
+var port = process.env.PORT || 8000;
+
+var requirejs = require('requirejs');
+
+http.createServer(function (request, response) {
+    console.log('request was made: ' + request.url);
+
+    if (request.url == '/' || request.url == '/index.html') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        var rs = fs.createReadStream('index.html', 'utf8');
+        rs.pipe(response);
+    }
+
+    if (request.url == '/defrequire.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript'
+        });
+        var rs = fs.createReadStream('defrequire.js', 'utf8');
+        rs.pipe(response);
+    }
+
+    if (request.url == '/main.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript'
+        });
+        var rs = fs.createReadStream('main.js', 'utf8');
+        rs.pipe(response);
+    }
+
+    if (request.url == '/favicon.ico') {
+        response.writeHead(200, {
+            'Content-Type': 'image/x-icon'
+        });
+        var rs = fs.createReadStream('favicon.ico', 'utf8');
+        rs.pipe(response);
+    }
+
+
+    if (request.url == '/node_modules/d3/dist/d3.min.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript'
+        });
+        var rs = fs.createReadStream('node_modules/d3/dist/d3.min.js', 'utf8');
+        rs.pipe(response);
+    }
+
+    if (request.url == '/node_modules/d3fc/build/d3fc.min.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript'
+        });
+        var rs = fs.createReadStream('node_modules/d3fc/build/d3fc.min.js', 'utf8');
+        rs.pipe(response);
+    }
+
+    if (request.url == '/node_modules/requirejs/require.js') {
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript'
+        });
+        var rs = fs.createReadStream('node_modules/requirejs/require.js', 'utf8');
+        rs.pipe(response);
+    }
+
+
 }).listen(port);
+
+console.log("Listening on 8000!")
+
+
+
